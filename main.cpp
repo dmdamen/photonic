c73e6d4108731bd240f8fa9843325c156c5438b3
Dennis Damen <dennis.damen@gmail.com>
2021-09-18 15:50:43 +0200

    pseado code for simpler version.

--- main.cpp
+++ main.cpp
@@ -26,7 +26,20 @@ std::string getFileExt(const std::string& s) {
    return("");
 }
 
-int main(void) {
+int main(int argc, char *argv[] ) {
+
+    // Parse & test commandline parameters
+
+    // Calculate sha256 for each file in the target folder
+        // try to add the file to a hash table. The sha256 is the key
+        // if add fails, add the file to an array of file that will be deleted
+        
+        // show green '+' sign if file is added to
+
+
+    // delete all files that are listed in the array of deletion candidates
+
+    // rename all remaining files to sha256.ext
 
     std::cout << "Photonic version " << PHOTONIC_VERSION_MAJOR << "." << PHOTONIC_VERSION_MINOR << std::endl;
 
