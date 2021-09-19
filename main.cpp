098a2b64dda99a25ebbe1bd5927dbe6a0bcb042f
Dennis Damen <dennis.damen@gmail.com>
2021-09-19 10:43:17 +0200

    Another test

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[] ) {
     std::cout << argc << std::endl;
 
     for (int i=0; i<argc; i++) {
-        std::cout << (std::string) argv[i];
+        std::cout << (std::string) argv[i] <<std::endl;
     }
 
     // Parse & test commandline parameters
