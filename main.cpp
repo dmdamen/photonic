227baa007fb57e9cac23fd918828b928f3c902b2
Dennis Damen <dennis.damen@gmail.com>
2021-09-25 10:47:32 +0200

    intermediate

--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@
 #include <openssl/md5.h>
 #include <vector>
 
+
+std::string get_fingerprint(std::filesystem::path path) {
+    return "Hello World";
+}
+
 int main(int argc, char *argv[] ) {
 
     std::cout << "Photonic version 1.0" << std::endl;
-    
+
     if (argc != 2) {
         std::cerr << "Please provide exactly one parameter" << std::endl;
         return 1;
@@ -22,6 +27,7 @@ int main(int argc, char *argv[] ) {
 
     for (const auto& p: std::filesystem::recursive_directory_iterator(std::filesystem::path(target_directory))) {
         if (!std::filesystem::is_directory(p)) {
+
             std::cout << p.path() << std::endl;
         }
     }
@@ -29,7 +35,7 @@ int main(int argc, char *argv[] ) {
     // Calculate sha256 for each file in the target folder
         // try to add the file to a hash table. The sha256 is the key
         // if add fails, add the file to an array of file that will be deleted
-        
+
         // show green '+' sign if file is added to
 
 
