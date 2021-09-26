eeb7933c9d88274319171876a9c28fd7c6106c4d
Dennis Damen <dennis.damen@gmail.com>
2021-09-26 09:28:44 +0200

    SHA works

--- main.cpp
+++ main.cpp
@@ -2,12 +2,46 @@
 #include <filesystem>
 #include <iostream>
 #include <map>
-#include <openssl/md5.h>
+#include <openssl/sha.h>
 #include <vector>
 
 
 std::string get_fingerprint(std::filesystem::path path) {
-    return "Hello World";
+
+    // Get file length
+    std::ifstream file(path, std::ios::binary | std::ios::ate);
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    // Initialize hash algoritm
+    SHA256_CTX *_sha256state = (SHA256_CTX *) malloc(sizeof(SHA256_CTX));
+    SHA256_Init(_sha256state);
+
+    // Read file into hashing algorithm
+    std::vector<char> buffer(size);
+    if (file.read(buffer.data(), size)) {
+        SHA256_Update(_sha256state, buffer.data(), size);
+    } else {
+        throw std::runtime_error(std::string("Could not open file for reading" + (std::string) path));
+    }  
+
+    file.close();
+
+    // Finalize hash
+    unsigned char *md = (unsigned char *) malloc(SHA256_DIGEST_LENGTH);
+    SHA256_Final(md,_sha256state);
+
+    // Format return string
+    std::stringstream returnStream;
+
+    for (int i=0; i < SHA256_DIGEST_LENGTH; i++) {
+		returnStream << std::setfill('0') 
+                     << std::setw(2)
+                     << std::hex
+                     << (0xff & (md[i]));
+	}
+
+	return returnStream.str();
 }
 
 int main(int argc, char *argv[] ) {
@@ -26,23 +60,21 @@ int main(int argc, char *argv[] ) {
         return 1;
     }
 
-    
-/*
-    std::map<std::string, std::string> fingerprints;
-    if (fingerprints.find("a") != fingerprints.end()) {
-        
-    }
-    fingerprints.insert(std::make_pair("a","a"));
-    fingerprints.insert(std::make_pair("a","a")); 
-*/
-
+    std::map<std::string, std::filesystem::directory_entry> keepers {};
+    std::vector<std::filesystem::directory_entry> fileToBeDeleted {};
     for (const auto& p: std::filesystem::recursive_directory_iterator(std::filesystem::path(target_directory))) {
         if (!std::filesystem::is_directory(p)) {
-
-            std::cout << p.path() << std::endl;
+            std::string fp = get_fingerprint(p.path());
+            if (keepers.find(fp) == keepers.end()) {
+                std::cout << "\u001b[32m+\u001b[0m " << fp << " " << p.path() << std::endl;
+                keepers.insert(std::make_pair(fp,p));
+            } else {
+                std::cout << "\u001b[31m-\u001b[0m " << fp << " " << p.path() << std::endl;
+                fileToBeDeleted.push_back(p);
+            }            
         }
     }
-
+  
     // Calculate sha256 for each file in the target folder
         // try to add the file to a hash table. The sha256 is the key
         // if add fails, add the file to an array of file that will be deleted
